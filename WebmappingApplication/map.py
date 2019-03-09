47e9478213c718556490ea497e449dfcb610931b
sudeshna <sudeshna@localhost.localdomain>
2019-03-10 04:00:04 +0530

    A Web Mapping application using Python Pandas and Folium

---
+++ WebmappingApplication/map.py
@@ -0,0 +1,35 @@
+import folium
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["Latitude"])
+lon = list(data["Longitude"])
+elev = list(data["Elevation"])
+
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000<=elevation<3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map=folium.Map(location=[22.48,88.34],zoom_start=1.5,tiles="Mapbox Bright")
+
+fgv=folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el in zip(lat,lon,elev):
+    #fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=7,popup=str(el) +"meter",icon=folium.Icon(color=color_producer(el))))
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=7,popup=str(el) +"meter",fill_color=color_producer(el),color='grey',fill_opacity=0.7))
+
+fgp=folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open("world.json",'r',encoding='utf-8-sig').read(),
+                style_function=lambda x:{'fillColor':'green'
+                if x['properties']['POP2005']<10000000 else 'orange' if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("map1.html")
